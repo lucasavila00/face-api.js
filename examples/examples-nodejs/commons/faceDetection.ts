f158c07f31853d8409784d82a71fafda415a16e4
Lucas de Ávila Martins <lucasdeavilamartins@Mac-mini-de-Lucas.local>
2019-12-16 10:06:42 -0300

    open cv solve pnp

--- examples/examples-nodejs/commons/faceDetection.ts
+++ examples/examples-nodejs/commons/faceDetection.ts
@@ -16,12 +16,12 @@ const minFaceSize = 50
 const scaleFactor = 0.8
 
 function getFaceDetectorOptions(net: faceapi.NeuralNetwork<any>) {
-  return net === faceapi.nets.ssdMobilenetv1
+  return (net as any) === faceapi.nets.ssdMobilenetv1
     ? new faceapi.SsdMobilenetv1Options({ minConfidence })
-    : (net === faceapi.nets.tinyFaceDetector
+    : ((net as any) === faceapi.nets.tinyFaceDetector
       ? new faceapi.TinyFaceDetectorOptions({ inputSize, scoreThreshold })
       : new faceapi.MtcnnOptions({ minFaceSize, scaleFactor })
     )
 }
 
-export const faceDetectionOptions = getFaceDetectorOptions(faceDetectionNet)
+export const faceDetectionOptions = getFaceDetectorOptions(faceDetectionNet as any)
--- examples/examples-nodejs/faceLandmarkDetection.ts
+++ examples/examples-nodejs/faceLandmarkDetection.ts
@@ -1,22 +1,31 @@
 import * as faceapi from 'face-api.js';
 
 import { canvas, faceDetectionNet, faceDetectionOptions, saveFile } from './commons';
+// let cv = require('./opencv.js')
 
 async function run() {
 
-  await faceDetectionNet.loadFromDisk('../../weights')
-  await faceapi.nets.faceLandmark68Net.loadFromDisk('../../weights')
+  await (faceDetectionNet).loadFromDisk('../../weights')
+  await (faceapi.nets.faceLandmark68Net).loadFromDisk('../../weights')
 
   const img = await canvas.loadImage('../images/bbt1.jpg')
   const results = await faceapi.detectAllFaces(img, faceDetectionOptions)
     .withFaceLandmarks()
 
-  const out = faceapi.createCanvasFromMedia(img) as any
+  const out = (faceapi).createCanvasFromMedia(img) as any
   faceapi.draw.drawDetections(out, results.map(res => res.detection))
   faceapi.draw.drawFaceLandmarks(out, results.map(res => res.landmarks))
 
   saveFile('faceLandmarkDetection.jpg', out.toBuffer('image/jpeg'))
   console.log('done, saved results to out/faceLandmarkDetection.jpg')
-}
+};
 
-run()
+run();
+
+// (global as any).Module = {
+//   onRuntimeInitialized() {
+//     // this is our application:
+//     console.log(cv.getBuildInformation())
+//     run()
+//   }
+// }
